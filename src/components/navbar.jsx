d5462e1b15081280aa1e3c9ba63d9ae199b96b0d
AlexisVS <alexis.vansan1440@gmail.com>
2025-03-13 18:32:13 +0100

    added Navbar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,16 +1,39 @@
 "use client";
 
 import {OptimizedImage} from "@/components/optimizedImage";
+import styled from "styled-components";
+import {NavbarButton} from "@/components/navigation/navbar-button";
+import {useState} from "react";
+
+const StyledNavbar = styled.div`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 1vh 1.77vw;
+`;
 
 export const Navbar = () => {
+    const [isOpen, setIsOpen] = useState(false);
+
     return (
-        <div className="navbar">
-            <div className="navbar-brand">
+        <StyledNavbar>
+            <div className="navbar__brand">
                 <OptimizedImage
+                    style={{width: "20vw"}}
                     src="/images/brand/brand_large.png"
                     alt="Logo de la marque"
+                    width="1994"
+                    height="974"
                 />
             </div>
-        </div>
+
+            <nav className={'navbar__navigation'}>
+
+            </nav>
+
+            <div className="navbar__menu__button">
+                <NavbarButton isOpen={isOpen} onClick={() => setIsOpen(!isOpen)}/>
+            </div>
+        </StyledNavbar>
     )
 }
