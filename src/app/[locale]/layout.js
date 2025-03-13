f94c76308c245a194284b872f92f8582a8991100
AlexisVS <alexis.vansan1440@gmail.com>
2025-03-13 12:55:47 +0100

    merged branches

--- src/app/[locale]/layout.js
+++ src/app/[locale]/layout.js
@@ -3,7 +3,6 @@ import {notFound} from 'next/navigation';
 import {routing} from '@/i18n/routing';
 import {setRequestLocale} from "next-intl/server";
 
-
 export function generateStaticParams() {
     return routing.locales.map((locale) => ({locale}));
 }
