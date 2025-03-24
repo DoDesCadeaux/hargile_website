ee2cb16deee32119c343822f52c17f5713a3458a
AlexisVS <alexis.vansan1440@gmail.com>
2025-03-25 00:00:13 +0100

    added trusted brands section

--- src/app/[locale]/page.jsx
+++ src/app/[locale]/page.jsx
@@ -2,12 +2,14 @@
 
 import Earth from '@/components/Earth';
 import OurSolutions from "@/components/pages/homepage/our-solutions/our-solutions";
+import TrustedBrandsContainer from "@/components/pages/homepage/trusted-brands/trusted-brands";
 
 export default function HomePage() {
     return (
         <>
             <Earth>
                 <OurSolutions/>
+                <TrustedBrandsContainer/>
             </Earth>
         </>
     );
