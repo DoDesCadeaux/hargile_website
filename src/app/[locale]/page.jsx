5febe43d5fc3aa3fadd0a9cdf3326e00ab9bfff6
MeHigh22 <72247500+MeHigh22@users.noreply.github.com>
2025-03-24 17:05:10 +0100

    AddedSomeStarsnShit

--- src/app/[locale]/page.jsx
+++ src/app/[locale]/page.jsx
@@ -2,11 +2,13 @@
 import Earth from "@/components/Earth";
 import { Navbar } from "@/components/navigation/navbar";
 import HeroSection from "@/components/pages/homepage/hero/heroSection";
+import Stars from "@/components/pages/homepage/Stars";
 
 export default function HomePage() {
   return (
     <>
       <Earth>
+        <Stars/>
         <HeroSection />
       </Earth>
     </>
