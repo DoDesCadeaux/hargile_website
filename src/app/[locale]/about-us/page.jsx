ea82b3655b8fab75fd955816592a9c4065a62912
AlexisVS <alexis.vansan1440@gmail.com>
2025-04-02 16:15:02 +0200

    added EarthVideoLayer.jsx

--- src/app/[locale]/about-us/page.jsx
+++ src/app/[locale]/about-us/page.jsx
@@ -1,6 +1,5 @@
 "use client";
 
-import Earth from '@/components/Earth';
 import OurStory from '@/components/pages/about-us/our-story/our-story';
 import OurTeam from "@/components/pages/about-us/our-team/our-team";
 import OurMission from "@/components/pages/about-us/our-mission/our-mission";
@@ -9,12 +8,12 @@ import OurCommitment from "@/components/pages/about-us/our-commitment/our-commit
 export default function AboutUsPage() {
     return (
         <>
-            <Earth>
-                <OurStory/>
-                <OurTeam/>
-                <OurMission/>
-                <OurCommitment/>
-            </Earth>
+            {/*<Earth>*/}
+            <OurStory/>
+            <OurTeam/>
+            <OurMission/>
+            <OurCommitment/>
+            {/*</Earth>*/}
         </>
     );
 }
---
+++ src/components/EarthVideoLayer.jsx
@@ -0,0 +1,88 @@
+"use client";
+
+import {useEffect, useState} from "react";
+import styled from "styled-components";
+
+const VideoContainer = styled.div`
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100vw;
+    height: 100vh;
+    overflow: hidden;
+    background-color: #000000;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`;
+
+const BackgroundVideo = styled.div`
+    position: relative;
+    max-width: 1920px;
+    max-height: 1080px;
+    width: 100%;
+    height: auto;
+    z-index: 2;
+    overflow: hidden;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    video {
+        width: 100%;
+        height: 100%;
+        object-fit: contain;
+        z-index: 2;
+    }
+`;
+
+const ParticlesWrapper = styled.div`
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    z-index: 1;
+`;
+
+const EarthVideoLayer = () => {
+    const [videoSrc, setVideoSrc] = useState("");
+
+    const selectVideoResolution = () => {
+        const width = window.innerWidth;
+
+        if (width >= 1920) {
+            return "/videos/earth/earth_1080.mp4";
+        } else if (width >= 1280) {
+            return "/videos/earth/earth_720.mp4";
+        } else {
+            return "/videos/earth/earth_540.mp4";
+        }
+    };
+
+    useEffect(() => {
+        const handleVideoResize = () => {
+            const selectedVideo = selectVideoResolution();
+            setVideoSrc(selectedVideo);
+        };
+
+        handleVideoResize();
+        window.addEventListener("resize", handleVideoResize);
+
+        return () => window.removeEventListener("resize", handleVideoResize);
+    }, []);
+
+    return (
+        <VideoContainer>
+            <BackgroundVideo>
+                {videoSrc && (
+                    <video autoPlay loop muted playsInline>
+                        <source src={videoSrc} type="video/mp4"/>
+                    </video>
+                )}
+            </BackgroundVideo>
+        </VideoContainer>
+    );
+};
+
+export default EarthVideoLayer;
