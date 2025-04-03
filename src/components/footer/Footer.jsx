9513cee4a739330083c1d43aba4eab947c34187d
AlexisVS <alexis.vansan1440@gmail.com>
2025-04-03 18:47:12 +0200

    other fixes

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -20,6 +20,13 @@ const Text = styled.p`
 
 const Copyright = styled.div`
     color: rgba(255, 255, 255, 0.7);
+    order: 1;
+    text-align: center;
+
+    @media (min-width: 640px) {
+        order: 2;
+        text-align: right;
+    }
 `;
 
 const Footer = () => {
--- src/components/footer/footer-container.styled.js
+++ src/components/footer/footer-container.styled.js
@@ -3,7 +3,7 @@ import styled from "styled-components";
 export const FooterContainerStyled = styled.footer`
     color: #fff;
     position: relative;
-    padding: 1rem 26px 4rem 12px;
+    padding: 1rem 26px 2rem 12px;
 
     @media (min-width: 1024px) {
         padding: 1rem clamp(26px, 8vw, 6vw) 4rem clamp(12px, 8vw, 6vw);
