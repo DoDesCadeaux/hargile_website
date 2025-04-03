806860ed2a6f4cd7c9a9159b72f5788a17e88028
AlexisVS <alexis.vansan1440@gmail.com>
2025-04-03 12:00:17 +0200

    fixed some inconsistency

--- src/components/footer/footer-container.styled.js
+++ src/components/footer/footer-container.styled.js
@@ -2,10 +2,10 @@ import styled from "styled-components";
 
 export const FooterContainerStyled = styled.footer`
     color: #fff;
-    padding: 4rem 2rem 1rem;
     position: relative;
+    padding: 4rem 12px 1rem;
 
     @media (min-width: 1024px) {
-        padding: 4rem 10vw 1rem;
+        padding: 4rem clamp(12px, 8vw, 6vw) 1rem;
     }
 `;
