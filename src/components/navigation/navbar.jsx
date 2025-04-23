bc66c55f6b2e8be2e0cd38598cb5b04bd7eb8edf
AlexisVS <alexis.vansan1440@gmail.com>
2025-04-23 16:15:31 +0200

    Add language selector and refactor navbar styling
    Implemented a language selector component with support for dynamic locale changes and styled it using styled-components. Extracted navbar styles into a separate file for better modularity and maintainability. Updated dependencies, including adding `country-flag-icons` and `typescript`.

--- src/components/navigation/navbar.jsx
+++ src/components/navigation/navbar.jsx
@@ -1,109 +1,31 @@
 "use client";
 
 import React, {memo, useEffect, useRef, useState} from "react";
-import styled from "styled-components";
 import NavbarButton from "@/components/navigation/button/navbar-button";
 import {useSiteNavigation} from "@/components/providers/site-navigation-provider";
 import {useRouter} from "@/i18n/navigation";
 import {OptimizedImage} from "@/components/optimizedImage";
 import {useMenuItems} from "@/hooks/useMenuItems";
 import {useNavigationVisibility} from "@/hooks/useNavigationVisibility";
-import {TransitionLink} from "@/components/TransitionLink";
 import {useTranslations} from "next-intl";
+import LanguageSelector from "@/app/[locale]/components/language-selector/language-selector";
+import {
+    Brand,
+    NavbarMenuButtons,
+    NavbarNavigation,
+    Spacer,
+    StyledLink,
+    StyledNavbar
+} from "@/components/navigation/navbar.styled";
 
-const StyledNavbar = styled.div`
-    display: flex;
-    position: fixed;
-    width: 100vw;
-    top: 0;
-    left: 0;
-    justify-content: space-between;
-    align-items: center;
-    padding: 0 24px 0 12px;
-    z-index: 10000;
-    backdrop-filter: blur(8px);
-`;
 
-const Brand = styled.button`
-    cursor: pointer;
-
-    &:hover {
-        box-shadow: none
-    }
-
-    &.transition::before {
-        content: '';
-        position: absolute;
-        width: 150vw;
-        height: 150vh;
-        top: -25vh;
-        left: -25vw;
-        background: rgba(0, 0, 0, 0);
-        z-index: 9999999;
-        animation: 2000ms appearInOut ease-in-out;
-    }
-
-    @keyframes appearInOut {
-        0% {
-            background: rgba(0, 0, 0, 0);
-        }
-        20% {
-            background: rgba(0, 0, 0, 1);
-        }
-        60% {
-            background: rgba(0, 0, 0, 1);
-        }
-        100% {
-            background: rgba(0, 0, 0, 0);
-        }
-    }
-`;
-
-const NavbarNavigation = styled.nav`
-    position: absolute;
-    top: 15vh;
-    left: 0;
-    width: 100vw;
-    height: 80vh;
-    max-height: 80vh;
-    padding-top: 10vh;
-    padding-bottom: 10vh;
-    overflow-y: auto;
-    flex-direction: column;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    gap: calc((15vh - 5vw) * 1);
-    z-index: 10001;
-    opacity: ${({$visible}) => $visible ? '1' : '0'};
-    transition: opacity 0.25s ease;
-    will-change: opacity;
-`;
-
-const StyledLink = styled(TransitionLink)`
-    text-decoration: none;
-    color: white;
-    font-size: clamp(30px, 15vw, 4vw);
-    cursor: pointer;
-    pointer-events: all;
-    font-weight: 650;
-    text-transform: uppercase;
-    opacity: 0;
-    transform: translateX(80px);
-    will-change: transform, opacity;
-`;
-
-const Spacer = styled.div`
-    width: 100%;
-`;
-
-const menuItems = [
+const menuItems = Array.from([
     {path: '/services', id: 'services'},
     // {path: '/solutions', id: 'solutions'},
     {path: '/about-us', id: 'our-dna'},
     {path: '/contact', id: 'contact'},
     {path: '/portfolio', id: 'portfolio'}
-];
+]);
 
 const Navbar = () => {
     const {isOpen, toggleMenu} = useSiteNavigation();
@@ -146,6 +68,8 @@ const Navbar = () => {
                         brandRef.current.classList.remove('transition');
                     }
                 }, 150);
+
+                clearTimeout(cleanupTimer)
             }, 150);
 
             return () => {
@@ -172,9 +96,10 @@ const Navbar = () => {
                     />
                 </Brand>
 
-                <div className="navbar__menu__button">
+                <NavbarMenuButtons>
+                    <LanguageSelector/>
                     <NavbarButton/>
-                </div>
+                </NavbarMenuButtons>
 
                 <NavbarNavigation
                     className="navbar-navigation"
