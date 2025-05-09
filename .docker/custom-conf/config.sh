853e4a4a7c66d8b2312fab1ab6ff54314a0495e0
AlexisVS <alexis.vansan1440@gmail.com>
2025-05-09 10:46:00 +0200

    Update acme.sh call to use full path for SSL issuance
    Replaced shorthand acme.sh invocation with full path to ensure consistent execution regardless of environment. Removed redundant source of .bashrc as it's no longer needed.

--- .docker/custom-conf/config.sh
+++ .docker/custom-conf/config.sh
@@ -2,10 +2,9 @@
 
 # Installer acme.sh
 curl https://get.acme.sh | sh
-source ~/.bashrc
 
-# Obtenir les certificats SSL
-acme.sh --issue --domain $DOMAIN --domain www.$DOMAIN --webroot /usr/local/lsws/Example/html/ --force
+# Obtenir les certificats SSL (utilisez le chemin complet)
+/root/.acme.sh/acme.sh --issue --domain $DOMAIN --domain www.$DOMAIN --webroot /usr/local/lsws/Example/html/ --force
 
 # Créer la configuration du virtual host
 mkdir -p /usr/local/lsws/conf/vhosts/$DOMAIN
